01fc0efd858877994efa20e506442d9606fc5d95
BlankBS <90792451+BlankBS@users.noreply.github.com>
2021-09-16 20:22:32 +0300

    Ex 3. p 165

---
+++ Task4/Task4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void massiv() 
+{
+	setlocale(LC_CTYPE, "rus");
+	const int masSize = 3;
+	float mas[masSize];
+	
+	cout << "Введите длинны сторон комнаты: ";
+
+	float Volume=1;
+
+	for (int i = 0; i < masSize; i++) {
+		cin >> mas[i];
+		Volume *= mas[i];
+	}
+
+	cout << "Объем вашей комнаты равен = " << Volume;
+}
+
+int main()
+{
+	massiv();
+	return 0;
+	system("pause");
+}
