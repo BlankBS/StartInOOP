e7573da2abd5403fd061e817f17d11d6d45b3636
BlankBS <90792451+BlankBS@users.noreply.github.com>
2021-09-16 19:45:58 +0300

    Ex. 1, p 165

--- StartInOOP/StartInOOP.cpp
+++ StartInOOP/StartInOOP.cpp
@@ -1,20 +1,78 @@
-﻿// StartInOOP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+void skipList() {
+	for (int i = 0; i <= 3; i++) {
+		cout << endl;
+	}
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void WorkWithNumber() {
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	setlocale(LC_CTYPE, "rus");
+	int firstPart;
+	cout << "Введите код города(***): ";
+	cin >> firstPart;
+	cout << endl;
+	if (firstPart <= 99 || firstPart >= 1000 || !cin) {
+		while (firstPart <= 99 || firstPart >= 1000) {
+			cout << "Введите правильное число(100-999): ";
+			cin >> firstPart;
+			if (!cin)
+			{
+				cin.clear();
+				while (cin.get() != '\n');
+			}
+		}
+	}
+
+
+	int secondPart;
+	cout << "Введите номер станции(***): ";
+	cin >> secondPart;
+	cout << endl;
+	if (secondPart <= 99 || secondPart >= 1000 || !cin) {
+		while (secondPart <= 99 || secondPart >= 1000) {
+			cout << "Введите правильное число(100-999): ";
+			cin >> secondPart;
+			if (!cin)
+			{
+				cin.clear();
+				while (cin.get() != '\n');
+			}
+		}
+	}
+
+	int thirdPart;
+	cout << "Введите номер абонента(****): ";
+	cin >> thirdPart;
+	cout << endl;
+	if (thirdPart <= 999 || thirdPart >= 10000 || !cin) {
+		while (thirdPart <= 999 || thirdPart >= 10000) {
+			cout << "Введите правильное число(1000-9999): ";
+			cin >> thirdPart;
+			if (!cin)
+			{
+				cin.clear();
+				while (cin.get() != '\n');
+			}
+		}
+	}
+
+	skipList();
+
+	cout << "Мой номер (212) 767-8900" << endl;
+	cout << "Ваш номер (" << firstPart << ")" << " " << secondPart << "-" << thirdPart;
+}
+
+void main()
+{
+	WorkWithNumber();
+	setlocale(LC_CTYPE, "rus");
+}
+/*
+Введите код города, номер станции и номер абонента:
+415 555 1212
+Мой номер (212) 767-8900
+Ваш номер (415) 555-1212
+*/
