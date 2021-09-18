0e18d4c8e448727e94e43e676bfabfea8bca6f65
BlankBS <90792451+BlankBS@users.noreply.github.com>
2021-09-18 15:44:06 +0300

    Big update!!!
    Make all taskes by "struct"

--- StartInOOP/StartInOOP.cpp
+++ StartInOOP/StartInOOP.cpp
@@ -1,78 +1,80 @@
 ﻿#include <iostream>
 using namespace std;
 
+struct myPhone {
+	int pMyFirst = 212;
+	int pMySecond = 767;
+	int pMyThird = 8900;
+};
+
+struct phone {
+	int pFirst;
+	int pSecond;
+	int pThird;
+};
+
 void skipList() {
 	for (int i = 0; i <= 3; i++) {
 		cout << endl;
 	}
 }
+void WorkWithMyNumber() {
+	setlocale(LC_CTYPE, "rus");
+
+	const int mySize = 3;
+	myPhone mas[mySize];
+	for (int i = 0; i < 1; i++) 
+	{
+		cout << "Мой номер (";
+		cout << mas[i].pMyFirst;
+		cout << ") ";
+		cout << mas[i].pMySecond;
+		cout << "-";
+		cout << mas[i].pMyThird;
+	}
+};
 
 void WorkWithNumber() {
+/*
+Введите код города, номер станции и номер абонента :
+415 555 1212
+Мой номер(212) 767 - 8900
+Ваш номер(415) 555 - 1212*/
 
 	setlocale(LC_CTYPE, "rus");
-	int firstPart;
-	cout << "Введите код города(***): ";
-	cin >> firstPart;
-	cout << endl;
-	if (firstPart <= 99 || firstPart >= 1000 || !cin) {
-		while (firstPart <= 99 || firstPart >= 1000) {
-			cout << "Введите правильное число(100-999): ";
-			cin >> firstPart;
-			if (!cin)
-			{
-				cin.clear();
-				while (cin.get() != '\n');
-			}
-		}
+
+	const int size = 3;
+	phone mas[size];
+	for (int i = 0; i < 1; i++) {
+		cout << "Введите код города, номер станции и номер абонента: " << endl;
+		cin >> mas[i].pFirst; 
+		cin >> mas[i].pSecond;
+		cin >> mas[i].pThird;
+		cout << "Ваш номер (" << mas[i].pFirst << ") ";
+		cout << mas[i].pSecond << "-" << mas[i].pThird;
+		cout << endl;
 	}
-
-
-	int secondPart;
-	cout << "Введите номер станции(***): ";
-	cin >> secondPart;
-	cout << endl;
-	if (secondPart <= 99 || secondPart >= 1000 || !cin) {
-		while (secondPart <= 99 || secondPart >= 1000) {
-			cout << "Введите правильное число(100-999): ";
-			cin >> secondPart;
-			if (!cin)
-			{
-				cin.clear();
-				while (cin.get() != '\n');
-			}
-		}
-	}
-
-	int thirdPart;
-	cout << "Введите номер абонента(****): ";
-	cin >> thirdPart;
-	cout << endl;
-	if (thirdPart <= 999 || thirdPart >= 10000 || !cin) {
-		while (thirdPart <= 999 || thirdPart >= 10000) {
-			cout << "Введите правильное число(1000-9999): ";
-			cin >> thirdPart;
-			if (!cin)
-			{
-				cin.clear();
-				while (cin.get() != '\n');
-			}
-		}
-	}
-
-	skipList();
-
-	cout << "Мой номер (212) 767-8900" << endl;
-	cout << "Ваш номер (" << firstPart << ")" << " " << secondPart << "-" << thirdPart;
 }
+
 
 void main()
 {
 	WorkWithNumber();
+	WorkWithMyNumber();
 	setlocale(LC_CTYPE, "rus");
 }
-/*
+/*Номер телефона, например (212) 767—8900, можно условно разделить на
+		три части: код города (212), номер телефонной станции (767) и номер
+		абонента (8900). Напишите программу с использованием структуры, поз-
+		воляющую раздельно хранить эти три части телефонного номера. Назо-
+		вите структуру phone. Создайте две структурные переменные типа phone.
+		Инициализацию одной из них произведите сами, а значения для другой
+		запросите с клавиатуры. Затем выведите содержимое обеих переменных
+		на экран. Результат работы программы должен выглядеть приблизитель-
+		но так:
 Введите код города, номер станции и номер абонента:
 415 555 1212
 Мой номер (212) 767-8900
 Ваш номер (415) 555-1212
+
 */
--- Task4/Task4.cpp
+++ Task4/Task4.cpp
@@ -2,27 +2,42 @@
 
 using namespace std;
 
-void massiv() 
+/*
+Создайте структуру с именем Volume, содержащую три поля типа Distance
+из примера englstrc, для хранения трех измерений помещения. Определи-
+те переменную типа Volume, инициализируйте ее, вычислите объем, зани-
+маемый помещением, и выведите результат на экран. Для подсчета объема
+переведите каждое из значений типа Distance в значение типа float, равное
+соответствующей длине в футах и хранимое в отдельной переменной. За-
+тем для вычисления объема следует перемножить три полученные веще-
+ственные переменные.
+*/
+struct Volume {
+	float length;
+	float width;
+	float height;
+};
+
+void Square() 
 {
 	setlocale(LC_CTYPE, "rus");
-	const int masSize = 3;
-	float mas[masSize];
-	
-	cout << "Введите длинны сторон комнаты: ";
+	float volume;
+	const int size = 3;
+	Volume mas[size];
 
-	float Volume=1;
-
-	for (int i = 0; i < masSize; i++) {
-		cin >> mas[i];
-		Volume *= mas[i];
+	for (int i = 0; i < 1; i++) {
+		cout << "Введите длинну, ширину и высоту: ";
+		cin >> mas[i].length;
+		cin >> mas[i].width;
+		cin >> mas[i].height;
+		volume = mas[i].height * mas[i].width * mas[i].length;
+		cout << "S = " << volume;
 	}
-
-	cout << "Объем вашей комнаты равен = " << Volume;
 }
 
 int main()
 {
-	massiv();
+	Square();
 	return 0;
 	system("pause");
 }
