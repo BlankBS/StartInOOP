2854cc46ab3915af08ef8c1c737ccc00f12fcb3e
BlankBS <90792451+BlankBS@users.noreply.github.com>
2021-09-18 20:32:10 +0300

    Ex2
    Next chapter

---
+++ Ex2/Ex2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void power()
+{
+    setlocale(LC_CTYPE, "rus");
+    float n;
+    int p;
+    cout << "Введите основание и показатель: ";
+    cin >> n >> p;
+    int n1;
+    n1 = n;
+    for (int i = 1; i < p; i++) {
+        n*=n1;
+    }
+    cout << n;
+}
+
+int main()
+{
+    power();
+}
