e959321da21e0c61f874504db545ac5ce5fbbf98
BlankBS <90792451+BlankBS@users.noreply.github.com>
2021-12-11 18:28:37 +0300

    Overload+strings
    10 tasks

---
+++ Inheritance/Inheritance.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "string"
+
+using namespace std;
+
+class publication {
+public:
+	string NameOfBook;
+	float CostOfBook;
+	class book {
+		int Pages;
+	};
+	class type {
+		float Minutes;
+	};
+};
+
+void getdataPublication() {
+	publication name;
+	for (int i = 0; i < 1; i++) {
+		cin >> name[i].publication;
+	}
+	
+}
+
+int main() {
+	setlocale(LC_CTYPE, "rus");
+}
